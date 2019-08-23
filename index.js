c2814ae3330e0ecb9da554496bbce917c1e305ae
Buford Eeds <bufordeeds8@gmail.com>
2019-08-23 17:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 // Add your functions and code here
+
 function destructivelyAppendKitten() {
   kittens.push('Ralph')
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift([0])
 }
