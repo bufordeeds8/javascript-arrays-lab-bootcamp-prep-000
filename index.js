605a82efbb5e392558b8d4c21c43d7df00c16885
Buford Eeds <bufordeeds8@gmail.com>
2019-08-23 18:02:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var moreKittens = kittens.push('Broom')
+  var moreKittens = name.push('Broom')
   return moreKittens
 }
